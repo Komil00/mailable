23a32cd668156669854270c91dd59557a57ff8ba
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2023-06-08 13:46:58 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,12 @@
+owner = 1520625615
+sudoers = [1520625615, 1775541139]
+domains = ["mailablebot.tk","user.mailable.me","themails.ml","treehub.live","digitalworm.tech","aquaseven.me","swiggie.space","crimpton.website"]
+reserved_keyword = [
+    'admin', 'owner', 'contact', 'support', 'donate', 'postmaster', 'message',
+    'ostrich', 'administrator', 'access', 'account', 'mail', 'email', 'patner',
+    'admins', 'porn', 'gov', 'government', 'noreply', 'subscribe', 'free',
+    'paid', 'api', 'auth', 'dev', 'developers', 'docs', 'faq', 'group',
+    'groups', 'help', 'hostmaster', 'join', 'local', 'login', 'master',
+    'register', 'root', 'terms', 'user', 'webmaster', 'quantum', 'theostrich',
+    'chat', 'melvik', 'storm', 'melvikstorm'
+]
